4b949c8b034af10b73149ce73af6da73e90cdb31
peixinwang <1131292790@qq.com>
2025-04-23 16:52:47 +0800

    49-通过ascii对单词进行分组，忽略了ascii相同，但是实际字母不同 需要通过排序后字符串完全相等来判断

--- 49.java
+++ 49.java
@@ -4,31 +4,21 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         // 把字符串分组，所有的字母除了顺序不相同，其他的都相同的分到一组
 
-        //  计算所有单词的权重，按照ascii计算
+        //  不同的单词，排序后完全相同，可以分为同一组
 
         // map存储value, List<String>
 
-        Map<Integer, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for(String s : strs){
             char[] ar = s.toCharArray();
-            int score = score(ar);
+            // 对字符串排序
+            Arrays.sort(ar);
         
-            List<String> list = map.getOrDefault(score, new ArrayList<>());
-            list.add(s);
-            map.put(score, list);
+            map.computeIfAbsent(new String(ar), k->new ArrayList<>()).add(s);
         }
 
         // map转为list
         return map.values().stream().toList();
     }
 
-    // 计算分数
-    public int score(char[] ar){
-        int num = 0;
-
-        for(char c : ar){
-            num+=c;
-        }
-        return num;
-    }
 }
