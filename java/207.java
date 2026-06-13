9c3b66a8b00db4c18f0361912acf188e86157212
peixinwang <1131292790@qq.com>
2026-06-13 21:11:51 +0800

    feat: 20260613刷题

--- java/207.java
+++ java/207.java
@@ -1,11 +1,15 @@
+/**
+ * @description: 课程表. 课程之间有依赖关系,根据依赖数组判断是可以拓扑排序
+ * 拓扑排序(判断这个有向图中是否存在环。 如果存在环，说明课程要求有冲突),不存在环(DAG)则可以完成所有课程
+ * numCourses = 2, prerequisites = [[1,0]] 输出: true
+ */
 class Solution {
     // 用dfs判断是否为DAG 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         // prerequisites[ai, bi]表示bi->ai的边
-        // 用邻接表表示图
-        // 表示每个节点做为起点的边的终点列表
-        // a1->[b1,b2]
-        // 构建图
+        // 用邻接表表示图,表示每个节点做为起点的边的终点列表
+
+        // 构建图(领接表)
         List<Integer>[] graph = new ArrayList[numCourses];
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
@@ -15,11 +19,11 @@ class Solution {
         }       
         
         // colors数组表示每个图节点的状态
-        // 3种颜色：0未访问，1访问中(dfs未返回)，2已访问(dfs已返回)
+        // 0未访问，1访问中(dfs未返回)，2已访问(dfs已返回)
         int[] colors = new int[numCourses];
         for(int i=0;i<numCourses;i++){
             // 找到一个没访问的点 开始dfs访问
-            // dfs返回true 就是找到了环
+            // dfs返回true 就是找到了环 拓扑排序失败
             if(colors[i] == 0 && dfs(i, graph, colors)){
                 return false;
             }    
@@ -28,10 +32,10 @@ class Solution {
     }
 
     /**
-      * dfs判断是否为DAG 三色标记法
+      * dfs判断是否存在环返回true则存在环,false则不存在环则是DAG
       * node 当前的节点
       * graph 图邻接表
-      * colors 节点状态数组
+      * colors 节点状态数组 (三色标记法)
       * return 是否为dag
      */
     private boolean dfs(int node, List<Integer>[] graph, int[] colors){
---
+++ java/common.java
@@ -0,0 +1,11 @@
+/**
+ * 这里定义的是公共类
+ */
+// 链表节点定义
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; 
+}
