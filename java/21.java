c203666a70af28ecf7d8da119c645e1585376ff4
peixinwang <1131292790@qq.com>
2026-07-12 20:58:35 +0800

    feat: 20260712刷题

--- java/21.java
+++ java/21.java
@@ -1,9 +1,13 @@
+/**
+ * @description: 合并两个有序的升序链表
+ */
 class Solution {
     // 时间O(n+m) 空间O(1)
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode prehead = new ListNode(-1);
+        ListNode dummy = new ListNode(-1);
 
-        ListNode prev = prehead;
+        // 代表合并后的链表的最后一个节点
+        ListNode prev = dummy;
         while (l1 != null && l2 != null) {
             if (l1.val <= l2.val) {
                 prev.next = l1;
@@ -18,6 +22,6 @@ class Solution {
         // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
         prev.next = l1 == null ? l2 : l1;
 
-        return prehead.next;
+        return dummy.next;
     }
 }
